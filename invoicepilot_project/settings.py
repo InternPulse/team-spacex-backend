472f78c161af226cdade083e558523fd16d59ae1
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-23 11:02:33 +0200

    Updated allowed_hosts

--- invoicepilot_project/settings.py
+++ invoicepilot_project/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS=localhost,127.0.0.1, team-spacex-backend-mbhhb.ondigitalocean.app
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['*', 'localhost', 'https://team-spacex-backend-mbhhb.ondigitalocean.app']
 
 
 # Application definition
