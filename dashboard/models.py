75350a1c9febd7b7792f5743e1c179b9df8ae31a
Tshegofatso Seane <55766279+tshegofatsoseane@users.noreply.github.com>
2024-01-31 09:53:18 +0200

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,13 +1,2 @@
 # dashboard/models.py
 from django.db import models
-from django.contrib.auth.models import User
-
-class DashboardData(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_receivables = models.IntegerField(default=0)
-    total_sales = models.IntegerField(default=0)
-    overdue_invoices = models.IntegerField(default=0)
-    total_receipts = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
-    def __str__(self):
-        return f"Dashboard Data for {self.user.username}"
