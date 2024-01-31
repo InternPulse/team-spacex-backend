d0a4c612fe8bea09145e2bb51d0474a598d2e96a
Tshegofatso Seane <55766279+tshegofatsoseane@users.noreply.github.com>
2024-01-31 02:34:36 +0200

    Update settings.py

--- invoicepilot_project/settings.py
+++ invoicepilot_project/settings.py
@@ -71,7 +71,8 @@ CORS_ALLOWED_ORIGINS=[
     "http://localhost:3000",
     "http://localhost:5175",
     "http://localhost:5173",
-    "http://localhost:8001"
+    "http://localhost:8001",
+    "https://team-spacex-frontend.netlify.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
