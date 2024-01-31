6ce9b45a58e012c48e6e4e3dce04d633465ed465
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-01-31 10:00:52 +0200

    updated dashboard models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+#dashboard/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import DashboardData
+
+admin.site.register(DashboardData)
